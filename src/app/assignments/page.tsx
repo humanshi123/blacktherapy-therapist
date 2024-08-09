855f14e9c20f5dc96ac83f6de13a9c3343a3fdde
humanshi123 <humanshi.auspicioussoft@gmail.com.com>
2024-08-09 17:23:42 +0530

    Home

--- src/app/assignments/page.tsx
+++ src/app/assignments/page.tsx
@@ -1,11 +1,108 @@
-import React from 'react';
+"use client"
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 const Page = () => {
-    return (
-        <div>
-            wellness
-        </div>
-    );
-}
+  const [activeTab, setActiveTab] = useState('peerSupport');
+  const [currentPage, setCurrentPage] = useState(0);
+  const rowsPerPage = 2; // Adjust the number of rows per page
+
+  const peerSupportData = [
+    { id: 1, client: 'Sandra Norton', date: '26 July 2023', phone: '7044431549', email: 'allisoncook58.ac@gmail.com', chat: 'Start Chat', videoChat: '', status: 'Confirm' },
+    { id: 2, client: 'Andrea Gibson', date: '26 July 2023', phone: '7044431549', email: 'allisoncook58.ac@gmail.com', chat: 'No Permission', videoChat: '', status: 'Confirm' },
+    // Add more dummy data here
+  ];
+
+  const therapistData = [
+    { id: 1, client: 'John Doe', date: '24 July 2023', phone: '7011234567', email: 'johndoe@gmail.com', chat: 'No Permission', videoChat: '', status: 'Confirm' },
+    { id: 2, client: 'Jane Smith', date: '25 July 2023', phone: '7027654321', email: 'janesmith@gmail.com', chat: 'Start Chat', videoChat: '', status: 'Confirm' },
+    { id: 3, client: 'John Doe', date: '24 July 2023', phone: '7011234567', email: 'johndoe@gmail.com', chat: 'No Permission', videoChat: '', status: 'Confirm' },
+    { id: 4, client: 'Jane Smith', date: '25 July 2023', phone: '7027654321', email: 'janesmith@gmail.com', chat: 'Start Chat', videoChat: '', status: 'Confirm' },
+    
+    // Add more dummy data here
+  ];
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+    setCurrentPage(0); // Reset to the first page when switching tabs
+  };
+
+  const handlePageClick = ({ selected }: { selected: number }) => {
+    setCurrentPage(selected);
+  };
+
+  const data = activeTab === 'peerSupport' ? peerSupportData : therapistData;
+  const filteredData = data.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
+
+  return (
+    <div className="">
+        <h1 className='mb-[50px]'>Assignmets</h1>
+      <div className="flex mb-[30px] gap-5">
+        <button className={`h-[46px] py-3 px-4 text-sm rounded-[5px] border border-[#283c63] ${activeTab === 'peerSupport' ? 'active bg-[#283c63] !text-white' : ''} text-[#26395e]`}
+         onClick={() => handleTabClick('peerSupport')}>
+          Peer Support Assignments
+        </button>
+        <button className={`h-[46px] py-3 px-4 text-sm rounded-[5px] border border-[#283c63] ${activeTab === 'therapist' ? 'active bg-[#283c63] !text-white' : ''} text-[#26395e]`}
+         onClick={() => handleTabClick('therapist')}>
+          Therapist Assignments
+        </button>
+      </div>
+<div className='table-common overflo-custom'>
+    
+<table className="">
+        <thead>
+          <tr className="">
+            <th>Client</th>
+            <th>Date Assigned</th>
+            <th>Phone Number</th>
+            <th>Email Address</th>
+            <th>Chat Client</th>
+            <th>Video Chat</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((item) => (
+            <tr key={item.id}>
+              <td>{item.client}</td>
+              <td>{item.date}</td>
+              <td>{item.phone}</td>
+              <td>{item.email}</td>
+              <td> 
+              <p className={`font-gothamMedium text-center rounded-3xl py-[2px] px-[10px] text-[10px] ${item.chat === 'Start Chat' ? ' text-[#42A803] bg-[#CBFFB2] ' : ' text-[#FFA234] bg-[#FFFCEC] '}`}>
+                {item.chat} </p>
+              </td>
+              <td>{item.videoChat}</td>
+              <td>
+                <p className='font-gothamMedium text-center rounded-3xl py-[2px] px-[10px] text-[10px]  text-[#42A803] bg-[#CBFFB2]'>{item.status}</p> 
+              </td>
+           </tr>
+          ))}
+        </tbody>
+      </table>
+
+</div>
+<div className="text-right">
+      <ReactPaginate
+        previousLabel={'<'}
+        nextLabel={'>'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={Math.ceil(data.length / rowsPerPage)}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={'inline-flex mt-[34px] rounded-[5px] border border-[#d5dce9]'}
+        pageClassName={'text-[#26395e] '}
+        pageLinkClassName={'py-2 px-4 inline-block'}
+        activeLinkClassName ={'text-white'}
+        activeClassName={'bg-[#26395e] rounded-[5px] text-white'}
+        previousLinkClassName={'py-2 px-4 inline-block text-[#26395e] '}
+        nextLinkClassName={'py-2 px-4 inline-block text-[#26395e] '}
+      />
+      </div>
+    </div>
+  );
+};
 
 export default Page;
--- src/app/payment-request/page.tsx
+++ src/app/payment-request/page.tsx
@@ -1,11 +1,98 @@
-import React from 'react';
+"use client";
+import React, {useState} from "react";
+import { ButtonArrow } from "@/utils/svgicon";
+import Image from 'next/image';
+import success from "../../assets/images/succes.png";
+
 
 const Page = () => {
-    return (
-        <div>
-            billing-iInsurance
-        </div>
-    );
-}
+  const [notification, setNotification] = useState<string | null>(null);
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevent the default form submission
+
+    // Show the notification
+    setNotification("Payment Request Submitted");
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000);
+  };
+  return (
+    <>
+      <h1 className=" mb-[25px] lg:mb-[50px]">
+      Payment Requests
+      </h1>
+      <div className=" bg-white rounded-[10px] w-full md:p-[40px] p-5">
+      <form onSubmit={handleSubmit}>
+          <div className="grid md:grid-cols-3 gap-[30px] ">
+            <div className="">
+              <label className="block mb-2">Requesters Name</label>
+              <input type="text" name="" id="" placeholder="John"/>
+            </div>
+            <div className="">
+              <label className="block mb-2">Email Address</label>
+              <input type="email" name="" id="" placeholder="loremipsum@gmail.com"/>
+            </div>
+            <div className="">
+              <label className="block mb-2">Request Type</label>
+              <select name="assignedClinician" value="" className="">
+                <option value="">--Select--</option>
+                <option value="Clinician 1">Clinician 1</option>
+                <option value="Clinician 2">Clinician 2</option>
+                <option value="Clinician 3">Clinician 3</option>
+              </select>
+            </div>
+            <div className="">
+              <label className="block mb-2">Services Provided</label>
+              <select name="assignedClinician" value="" className="">
+                <option value="">--Select--</option>
+                <option value="Clinician 1">Clinician 1</option>
+                <option value="Clinician 2">Clinician 2</option>
+                <option value="Clinician 3">Clinician 3</option>
+              </select>
+            </div>
+            <div>
+              <label className="block mb-2">Client Name</label>
+              <input type="text" name="" id="" placeholder=""/>
+            </div>
+            <div>
+              <label className="block mb-2">Date of Services Provided</label>
+              <input type="date" name="" id="" placeholder=""/>
+            </div>
+          </div>
+          <div className="flex flex-wrap gap-[30px] mt-[30px]">
+          <div className="md:w-[calc(20%-15px)]">
+              <label className="block mb-2">Time of Services Provided</label>
+              <input type="time" name="" id="" placeholder=""/>
+            </div>
+            <div className="md:w-[calc(20%-15px)]">
+              <label className="block mb-2">Duration (Hours)</label>
+              <input type="time" name="" id="" placeholder=""/>
+            </div>
+            <div className="md:w-[calc(60%-30px)]">
+              <label className="block mb-2">Progress Notes and/or Assessments Submitted & Approved?</label>
+              <select name="assignedClinician" value="" className="">
+                <option value="">--Select--</option>
+                <option value="Clinician 1">Clinician 1</option>
+                <option value="Clinician 2">Clinician 2</option>
+                <option value="Clinician 3">Clinician 3</option>
+              </select>
+            </div>
+          </div>
+         <div className='mt-[30px] flex justify-end '>
+         <button type="submit" className="button px-[30px]">Submit<ButtonArrow /> </button>
+         </div>
+        </form>
+        {notification && (
+        <div className="fixed inset-0 grid place-items-center w-full h-full bg-gray-500 bg-opacity-75">
+          <div className="bg-white text-[#283C63] py-[60px] rounded-[20px] shadow-lg max-w-[584px] w-full">
+            <Image src={success} alt="success" height={130} width={115} className="mx-auto" />
+            <h2 className="text-center mt-[40px]">{notification}</h2>
+          </div>
+        </div>
+      )}
+      </div>
+    </>
+  );
+};
 export default Page;
